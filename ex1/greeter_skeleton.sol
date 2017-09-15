61d1a0c0319cb9d430ffda1fba9aa6dead4b0d2e
ALEKSANDAR IVANOV <tsarsandrbasileus@berkeley.edu>
2017-09-14 18:43:29 -0700

    added draft of greeter

--- ex1/greeter_skeleton.sol
+++ ex1/greeter_skeleton.sol
@@ -7,9 +7,10 @@ contract greeter {
 
 	function greeter(string _greeting) public {
 		/* Write one line of code for the contract to set our greeting */
+		greeting = _greeting;
 	}
 
-	function greet() constant returns (string)  {
+	function greet() constant returns (string greeting)  {
 		/* Write one line of code to allow the contract to return our greeting */
 	}
 }
