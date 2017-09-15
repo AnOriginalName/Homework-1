92b1d403e942be83580d0cb99e6fe6a65a166942
ALEKSANDAR IVANOV <tsarsandrbasileus@berkeley.edu>
2017-09-14 19:41:23 -0700

    finished greeter

--- ex1/greeter_skeleton.sol
+++ ex1/greeter_skeleton.sol
@@ -3,14 +3,15 @@ pragma solidity ^0.4.13;
 
 contract greeter {
 	/* Add one variable to hold our greeting */
-	string greeting;
+		string greeting;
 
-	function greeter(string _greeting) public {
-		/* Write one line of code for the contract to set our greeting */
-		greeting = _greeting;
-	}
+			function greeter(string _greeting) public {
+					/* Write one line of code for the contract to set our greeting */
+							greeting = _greeting;
+								}
 
-	function greet() constant returns (string greeting)  {
-		/* Write one line of code to allow the contract to return our greeting */
-	}
-}
+									function greet() constant returns (string)  {
+											/* Write one line of code to allow the contract to return our greeting */
+													return greeting;
+														}
+														}
