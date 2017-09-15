1ba4e26d434121082908070992384e0946903bb0
ALEKSANDAR IVANOV <tsarsandrbasileus@berkeley.edu>
2017-09-14 20:10:24 -0700

    finished ex2

--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -1,10 +1,25 @@
 pragma solidity ^0.4.16;
 
+contract Ficonacci {
+    uint x;
+        uint pos;
+	    function calculate(uint _position) returns (uint result) {
+	            /*Add one variable to hold our greeting*/
+		            if (_position == 0) {
+			                result = 0;
+					        }
+						        else if (_position == 1) {
+							            result = 1;
+								            } else {
+									                result = calculate(_position - 1) + calculate(_position - 2);
+											        }
 
-contract Fibonacci {
-	function calculate(uint position) returns (uint result) {
-		/* Add one variable to hold our greeting */
-	}
+													    }
+													        /*Add a falback fnction to prevent contract payability and non
+														    existant function calls*/
+														        function() {
+															        x = 1;
+																    }
 
-	/* Add a fallback function to prevent contract payability and non-existent function calls */
-}
+
+																	    }
